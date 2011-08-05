05cae08d6e4bba0753d09df75e255818c275d7c2
Telmo Menezes <telmo@telmomenezes.com>
2011-08-05 16:21:02 +0200

    added author_citations to trimyears.py

--- trimyears.py
+++ trimyears.py
@@ -47,21 +47,30 @@ def trimyears(dbpath, begin_year, end_year):
     cur.execute("SELECT count(id) FROM articles")
     row = cur.fetchone()
     articles = row[0]
+    cur.execute("SELECT count(id) FROM author_citations")
+    row = cur.fetchone()
+    author_citations = row[0]
     cur.execute("SELECT count(id) FROM issues WHERE timestamp<? or timestamp>?", (ts0, ts1))
     row = cur.fetchone()
     discard_issues = row[0]
     cur.execute("SELECT count(id) FROM articles WHERE timestamp<? or timestamp>?", (ts0, ts1))
     row = cur.fetchone()
     discard_articles = row[0]
+    cur.execute("SELECT count(id) FROM author_citations WHERE timestamp<? or timestamp>?", (ts0, ts1))
+    row = cur.fetchone()
+    discard_author_citations = row[0]
 
     issue_per = float(discard_issues) / float(issues) * 100.0
     article_per = float(discard_articles) / float(articles) * 100.0
+    author_cit_per = float(discard_author_citations) / float(author_citations) * 100.0
 
     print('Of %d total issues, %d will be discarded. (%.2f%%)' % (issues, discard_issues, issue_per))
     print('Of %d total articles, %d will be discarded. (%.2f%%)' % (articles, discard_articles, article_per))
+    print('Of %d total author citations, %d will be discarded. (%.2f%%)' % (author_citations, discard_author_citations, author_cit_per))
 
     cur.execute("DELETE FROM issues WHERE timestamp<? or timestamp>?", (ts0, ts1))
     cur.execute("DELETE FROM articles WHERE timestamp<? or timestamp>?", (ts0, ts1))
+    cur.execute("DELETE FROM author_citations WHERE timestamp<? or timestamp>?", (ts0, ts1))
 
     conn.commit()
     cur.close()
